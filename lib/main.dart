0f763d479434e835eef65c9239433183bd790956
AbuBakar044 <bhattiabubakar@gmail.com>
2023-09-14 18:33:30 +0500

    Attached Firebase
    Attached Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:my_online_buddies/screens/splash/splash_screen.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
